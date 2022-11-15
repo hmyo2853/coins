667f99743c3bb2c8ad89c395e17c2d73aa0c300e
hmyo2853 <hmyo2853@gmail.com>
2022-11-15 23:16:32 +0900

    초기 api 받아오는 세팅 완료

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import styled from "styled-components";
 
 function App() {
+  const [isLoading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    const getData = async () => {
+      await axios
+        .get("https://api.coinpaprika.com/v1/tickers?quotes=KRW")
+        .then((items) => {
+          setCoins(items.data.slice(0, 100));
+          setLoading(false);
+        })
+        .catch((error) => {
+          console.log("error type : ", error);
+          return null;
+        });
+    };
+    getData();
+  }, []);
+  console.log(coins);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Text>{isLoading ? "loading...." : `${coins.length}`}</Text>
+      <Container>
+        {isLoading ? (
+          <strong>Loading...</strong>
+        ) : (
+          <div>
+            {coins.map((coin) => (
+              <p>
+                {coin.id} ({coin.symbol})
+              </p>
+            ))}
+          </div>
+        )}
+      </Container>
+    </>
   );
 }
+
+const Text = styled.div`
+  display: flex;
+  font-size: 2rem;
+  font-weight: 600;
+`;
+
+const Container = styled.div``;
 
 export default App;
